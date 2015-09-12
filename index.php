b4a3a801605d1e1853e077c08392ad40fcf67114
Aesthetickz <n.c.gerritsen@gmail.com>
2015-09-12 20:19:13 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta http-equiv=X-UA-Compatible content="IE=edge">
+<title>CareHR</title>
+<meta name=description content="">
+<meta name=viewport content="width=device-width">
+<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+<link rel=stylesheet href="styles/vendor/bootstrap.min.css">
+<link rel=stylesheet href="styles/style.css">
+
+<body data-spy="scroll" data-target=".main-menu" data-offset="-80px">
+<!--[if lt IE 10]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+<div class="container-fluid">
+
+    <div class="row" id="top-bar">
+        <div class="wrap">
+            <div class="col-sm-4 col-xs-6 logo-container">
+                <a href="#home"><img src="images/carehr-logo.png" alt="" class="logo"></a>
+            </div>
+            <nav class="col-sm-8 cols-xs-6 main-menu">
+            <ul class="nav">
+                <li class="hidden"><a href="#home">Home</a></li>
+                <li><a href="#diensten">Diensten</a></li>
+                <li><a href="#over">Over</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+            <div class="mobile">
+                <p class="instruction">Scroll Down &#8595;</p>
+            </div>
+            </nav>
+        </div>
+    </div>
+    <header class="row" id="home">
+    <div class="wrap">
+    </div>
+    </header>
+    <div class="row" id="intro">
+        <div class="wrap">
+            <div class="col-sm-6 clearfix">
+                <h2>HR Advies in de Zorg</h2>
+                <p>
+                    “Mijn visie is dat de beste bedrijfsresultaten worden gerealiseerd in een omgeving waar medewerkers de ruimte krijgen te excelleren.”<br>
+ “Ik ben een steunpilaar voor management en zoek vertrouwen bij medewerkers door een transparante wijze van communiceren en het zoeken naar creatieve oplossingen.”
+                </p>
+                <!--<a href="" class="button">Lees Meer</a>-->
+            </div>
+            <div class="col-sm-6">
+                <img class="fill" src="images/care-icons.png">
+            </div>
+        </div>
+    </div>
+    <div class="row" id="diensten">
+        <div class="wrap">
+            <h2>Diensten</h2>
+
+            <div class="dienst clearfix">
+                <img src="images/diensten/health-balls.png" alt="">
+                <h3>HR Advies en Ondersteuning in de Zorg</h3>
+                <p class="intro">Door mijn vele jaren ervaring in de zorg ben ik snel inzetbaar. Dat kan zijn voor vragenstukken als functiewaardering, functiebeschrijving, werving en selectie recruiting maar ook voor projecten.</p>
+                <p>Ik heb expertise op het gebied van loopbaanontwikkeling,  competentie- management, invoering van E-HRM, bijvoorbeeld bij het jaargesprek of E-learning. Ik kan u adviseren op het gebeid van formatiebeheer, een formatieplaatsenplan opstellen en u helpen bij de inrichting van een personeelsinformatiesysteem.</p>
+            </div>
+            <div class="dienst clearfix">
+                <img src="images/diensten/care-guys.png" alt="">
+                <h3>Ondersteuning en juridisch advies voor zorgmedewerkers</h3>
+                <p class="intro">De Zorg heeft te maken met ingrijpende veranderingen. Functies vervallen of veranderen. Sociale plannen worden opgesteld.</p>
+                <p>Als lid van de Commissie Sociale Begeleiding Vlietland Ziekenhuis, voorzitter Commissie Sociale Begeleiding Rijndam revalidatiecentrum te Rotterdam en voorzitter IBC van diverse ziekenhuizen in Den Haag heb ik veel expertise opgebouwd om veranderingsprocessen te beoordelen en te adviseren over processen. Voorkomen is beter dan genezen.  Ik ondersteun u graag in de wetenschap dat ik een toegevoegde waarde kan hebben voor mens en organisatie </p>
+            </div>
+            <div class="dienst clearfix">                
+                <img src="images/diensten/suitcase.png" alt="">
+                <h3>Interim HR opdrachten</h3>
+                <p class="intro">Is uw manager P&O of uw HR-adviseur voor een langere periode uitgeschakeld? Door mijn brede ervaring ben ik in staat werkzaamheden snel over te nemen. Ik ben beschikbaar voor 3 à 4 dagen in de week.<br>
+                Heeft u behoefte uw afdeling P&O, na vertrek van de manager, opnieuw in te richten?</p>
+                <p>Ik kan de bestaande werkzaamheden voor u blijven uitvoeren en een scan maken van sterke en zwakke punten. Op basis hiervan kan ik een verbeterplan implementeren en ondertussen met u zoeken naar een nieuwe manager of voorbereidingen treffen indien u P&O wil samenvoegen met een ander organisatieonderdeel.</p>
+            </div>
+            <div class="dienst clearfix">
+                <img src="images/diensten/interim-search.png" alt="">
+                <h3>Bureau Interim management zorgmanagers</h3>
+                <p></p>
+            </div>
+        </div>
+    </div>
+    <div class="row" id="over">
+        <div class="wrap">
+            <h2>Over</h2>
+            <p>Voor meer informatie over mij, zie:</p>
+            <a href="http://www.carloreeser.nl" class="button">carloreeser.nl</a>
+        </div>
+    </div>
+
+    <?php include 'contact-form.php'; ?>
+    
+</div>
+<script src="scripts/vendor/jquery.min.js"></script>
+<script src="scripts/vendor/jquery.validate.min.js"></script>
+<script src="scripts/vendor/bootstrap.scrollspy.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');</script>
+
+<script src="scripts/main.js"></script>
---
+++ scripts/main.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+    $("body").scrollspy({
+        target: ".main-menu"
+    });
+
+    $("a[href*=#]:not([href=#]):not(a.flex-next):not(a.flex-prev)").click(function () {
+        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") || location.hostname == this.hostname) {
+            var a = $(this.hash);
+            if (a = a.length ? a : $("[name=" + this.hash.slice(1) + "]"), a.length) return $("html,body").animate({
+                scrollTop: a.offset().top - 68
+            }, 1e3), !1;
+        }
+    });
+
+    $('#contact-form').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            subject: {
+                required: true,
+                minlength: 2
+            },
+            message: {
+                required: true,
+                minlength: 10
+            }
+        },
+        messages: {
+            name: {
+                required: "Vul alstublieft een naam in!",
+                minlength: "Uw naam lijkt te kort te zijn!"
+            },
+            email: {
+                required: "Vul alstublieft een email adres in!",
+                email: "Dit email adres is niet geldig!"
+            },
+            subject: {
+                required: "Vul alstublieft een onderwerp in!",
+                email: "Dit onderwerp lijkt te kort!"
+            },
+            message: {
+                required: "Vul alstublieft een bericht in!",
+                minlength: "Uw bericht moet minimaal 10 tekens bevatten."
+            }
+        }
+    });
+
+    $('.close-message').click(function(){
+        $('.message-container').fadeOut(500);
+    });
+
+    
+});
+
+$(document).on("scroll",function(){
+    if($(document).scrollTop()>150){
+        $('#top-bar').addClass("narrow");
+    } else{
+        $('#top-bar').removeClass("narrow");
+    }
+});
