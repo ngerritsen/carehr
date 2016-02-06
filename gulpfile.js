e9c1a4ef56db84426bf648e62d621de250528b41
Niels Gerritsen <n.gerritsen@coolblue.nl>
2016-02-06 14:40:39 +0100

    Added travis build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var ftp = require('vinyl-ftp');
+var gutil = require('gulp-util');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var minimist = require('minimist');
+
+var args = minimist(process.argv.slice(2));
 
 gulp.task('sass', function () {
   return gulp.src('./styles/sass/style.scss')
@@ -18,6 +23,29 @@ gulp.task('sass', function () {
 
 gulp.task('default', function () {
   gulp.watch('./styles/sass/**/*.scss', ['sass'])
+});
+
+gulp.task('deploy', function () {
+  var remotePath = '/public_html/';
+  var conn = ftp.create({
+    host: 'carehr.nl',
+    user: args.user,
+    password: args.password,
+    log: gutil.log
+  });
+
+  var files = [
+    'index.php',
+    'contact-form.php',
+    './**/*.css',
+    './styles/fonts/',
+    './scripts/'
+    './images/'
+  ];
+
+  gulp.src(files)
+    .pipe(conn.newer(remotePath))
+    .pipe(conn.dest(remotePath));
 });
 
 gulp.task('build', ['sass']);
