f49b94fe9d0884aae48da801aee5b3f76adaac59
Niels Gerritsen <n.c.gerritsen@gmail.com>
2018-01-13 20:30:21 +0100

    Add services

--- data.js
+++ data.js
@@ -18,5 +18,22 @@ module.exports = {
       image: 'fred.jpg',
       linkedin: 'https://www.linkedin.com/in/frederik-fred-spiering-520b3a6/'
     }
+  ],
+  services: [
+    {
+      title: 'HR Advies en Ondersteuning in de Zorg',
+      intro: 'Door mijn vele jaren ervaring in de zorg ben ik snel inzetbaar. Dat kan zijn voor vragenstukken als functiewaardering, functiebeschrijving, werving en selectie recruiting maar ook voor projecten.',
+      content: 'Ik heb expertise op het gebied van loopbaanontwikkeling, competentie- management, invoering van E-HRM, bijvoorbeeld bij het jaargesprek of E-learning. Ik kan u adviseren op het gebeid van formatiebeheer, een formatieplaatsenplan opstellen en u helpen bij de inrichting van een personeelsinformatiesysteem.'
+    },
+    {
+      title: 'Ondersteuning en juridisch advies voor zorgmedewerkers',
+      intro: 'De Zorg heeft te maken met ingrijpende veranderingen. Functies vervallen of veranderen. Sociale plannen worden opgesteld.',
+      content: 'Als lid van de Commissie Sociale Begeleiding Vlietland Ziekenhuis, voorzitter Commissie Sociale Begeleiding Rijndam revalidatiecentrum te Rotterdam en voorzitter IBC van diverse ziekenhuizen in Den Haag heb ik veel expertise opgebouwd om veranderingsprocessen te beoordelen en te adviseren over processen. Voorkomen is beter dan genezen. Ik ondersteun u graag in de wetenschap dat ik een toegevoegde waarde kan hebben voor mens en organisatie'
+    },
+    {
+      title: 'Interim HR opdrachten',
+      intro: 'Is uw manager P&O of uw HR-adviseur voor een langere periode uitgeschakeld? Door mijn brede ervaring ben ik in staat werkzaamheden snel over te nemen. Ik ben beschikbaar voor 3 à 4 dagen in de week. Heeft u behoefte uw afdeling P&O, na vertrek van de manager, opnieuw in te richten?',
+      content: 'Ik kan de bestaande werkzaamheden voor u blijven uitvoeren en een scan maken van sterke en zwakke punten. Op basis hiervan kan ik een verbeterplan implementeren en ondertussen met u zoeken naar een nieuwe manager of voorbereidingen treffen indien u P&O wil samenvoegen met een ander organisatieonderdeel.'
+    }
   ]
 }
---
+++ src/validation.js
@@ -0,0 +1,102 @@
+'use strict';
+
+ready(() => {
+  const form = document.querySelector('[data-form-validation]');
+  const formSections = [...document.querySelectorAll('[data-form-section]')];
+
+  form.addEventListener('submit', (event) => {
+    const errors = validateForm(formSections);
+
+    if (errors) {
+      event.preventDefault();
+    }
+  })
+});
+
+function validateForm(formSections) {
+  const name = getFormInput(formSections, 'name');
+  const email = getFormInput(formSections, 'email');
+  const subject = getFormInput(formSections, 'subject');
+  const message = getFormInput(formSections, 'message');
+
+  let errors = false;
+
+  formSections.forEach(unsetError);
+
+  if (name.value.trim().length < 3) {
+    setError(name.container, 'Vul een naam in van minimaal 3 karakters in.');
+    errors = true;
+  }
+
+  if (!isValidEmail(email.value)) {
+    setError(email.container, 'Vul een geldig email adres in.');
+    errors = true;
+  }
+
+  if (subject.value.trim().length < 3) {
+    setError(subject.container, 'Vul een onderwerp van minimaal 3 karakters in.');
+    errors = true;
+  }
+
+  if (message.value.trim().length < 21) {
+    setError(message.container, 'Vul een bericht van minimaal 21 karakters in.');
+    errors = true;
+  }
+
+  return errors;
+}
+
+function unsetError(container) {
+  const errorEl = container.querySelector('[data-form-error]');
+
+  if (errorEl) {
+    container.removeChild(errorEl);
+  }
+}
+
+function setError(container, message) {
+  const errorEl = container.querySelector('[data-form-error]');
+
+  if (errorEl) {
+    errorEl.textContent = message;
+    return;
+  }
+
+  container.appendChild(createErrorEl(message));
+}
+
+function createErrorEl(message) {
+  const el = document.createElement('div');
+
+  el.textContent = message;
+  el.classList.add('form-error');
+
+  return el;
+}
+
+function isValidEmail(value) {
+  return !!value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/);
+}
+
+function getFormInput(formSections, name) {
+  const el = formSections.find(el => el.getAttribute('data-form-section') === name);
+  const field = el.querySelector('input') || el.querySelector('textarea');
+
+  return {
+    container: el,
+    value: field.value
+  }
+}
+
+function ready(fn) {
+  if (
+    document.attachEvent
+      ? document.readyState === 'complete'
+      : document.readyState !== 'loading'
+  ) {
+    fn();
+    return;
+  }
+
+  document.addEventListener('DOMContentLoaded', fn);
+}
