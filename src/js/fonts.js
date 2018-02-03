4707f2c7d953a714bb3c591b43a32dcfe66b4f25
Niels Gerritsen <n.c.gerritsen@gmail.com>
2018-02-03 11:16:09 +0100

    Improved front loading

---
+++ src/js/fonts.js
@@ -0,0 +1,5 @@
+import WebFont from 'webfontloader';
+
+export default function loadWebFonts() {
+  WebFont.load(window.webFontConfig);
+}
--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,9 @@ import initContactForm from './contactForm';
 import initIcons from './icons';
 import initNotifications from './notifications';
 import ready from './ready';
+import loadWebFonts from './fonts';
 
+loadWebFonts();
 initIcons();
 
 ready(() => {
